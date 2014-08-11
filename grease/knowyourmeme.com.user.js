3e4c897c1b1cfbcea6a0a19a3c0c9b18dcd4a2cd
Nick <nmapsy@gmail.com>
2014-08-11 16:21:42 -0700

    grease/knowyourmeme.com.user.js: debugging, but working version

--- grease/knowyourmeme.com.user.js
+++ grease/knowyourmeme.com.user.js
@@ -5,8 +5,30 @@
 // @include     https://knowyourmeme.com/*
 // @include     http://apis.google.com/*
 // @include     https://apis.google.com/*
-// @version     1.0
+// @run-at      document-end
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 $('.floating-bar.floating-fixed')[0].style.visibility = 'hidden';
-$('.g-plusone-wrapper iframe')[0].style.visibility = 'hidden'; // G+ icon
+//$('.g-plusone-wrapper iframe')[0].style.visibility = 'hidden'; // G+ icon
+
+var e = document.getElementsByClassName('g-plusone-wrapper')[0];
+var children = e.children;
+var message = e.tagName+":";
+for (var i = 0; i < children.length; i++) {
+  message += "\n  "+children[i].tagName;
+}
+alert(message);
+
+e = children[0];
+var children = e.children;
+var message = e.tagName+":";
+for (var i = 0; i < children.length; i++) {
+  message += "\n  "+children[i].tagName;
+}
+alert(message);
+
+e = children[0];
+alert(e.tagName+': "'+e.style.visibility+'"');
+e.style.visibility = 'hidden';
+alert(e.tagName+': "'+e.style.visibility+'"');
