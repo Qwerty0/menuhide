43710e925be3aa2082d16bd1b10e1960949a4046
Nick <nmapsy@gmail.com>
2014-08-11 17:26:12 -0700

    bookmarklet-bare.js: remove, no longer necessary

--- bookmarklet-bare.js
+++
@@ -1,8 +0,0 @@
-String.prototype.endsWith = function(suffix) {
-  return this.indexOf(suffix, this.length - suffix.length) !== -1;
-};
-if (document.domain.endsWith('twitter.com')) {
-  $('.topbar.js-topbar')[0].style.visibility = 'hidden';
-} else if (document.domain.endsWith('knowyourmeme.com')) {
-  $('.g-plusone-wrapper iframe')[0].style.visibility = 'hidden';
-}
