8cf32a0370301bb7931fef657d96cc8e9d88160c
Nick <nmapsy@gmail.com>
2014-08-19 10:30:10 -0700

    greasemonkey: Mashable

---
+++ grease/mashable.com.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Mashable
+// @namespace   Menu Hide
+// @include     http://mashable.com/*
+// @include     https://mashable.com/*
+// @exclude     http://mashable.com/
+// @exclude     https://mashable.com/
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+document.getElementById('site-header').style.visibility = 'hidden';
+
