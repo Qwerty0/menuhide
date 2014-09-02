fed61a21ac89a17ba5ade446ff3dfebbfb4fa55a
Nick <nmapsy@gmail.com>
2014-09-02 12:37:15 -0400

    grease: add Funny or Die

---
+++ grease/funnyordie.com.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Funny or Die
+// @namespace   Menu Hide
+// @include     http://www.funnyordie.com/*
+// @include     https://www.funnyordie.com/*
+// @exclude     http://www.funnyordie.com/
+// @exclude     https://www.funnyordie.com/
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+document.getElementById('page-header').style.visibility = 'hidden';
